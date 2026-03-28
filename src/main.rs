c0aaad80beb84816aa576281474704f75439f87d
汝吾此便安好 <sonder.qicat@outlook.com>
2026-03-28 23:55:17 +0800

    向前端发送数据

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,21 @@
-use clap::Parser;
-use rcli::{Opts, SubCommand, process_csv, process_genpass, Base64SubCommand, process_encode, process_decode};
-fn main() -> anyhow::Result<()> {
-    let opts = Opts::parse();
-    match opts.cmd {
-        SubCommand::Csv(opts) => {
-            let output = if let Some(output) = opts.output {
-                output.clone()
-            } else {
-                format!("output.{}", opts.format)
-            };
-            // 命名尽量要简单
-            process_csv(&opts.input, &output, opts.format)?;
-        }
-        SubCommand::GenPass(opts) => {
-            process_genpass(&opts)?;
-        }
-        SubCommand::Base64(subcommand) => match subcommand {
-            Base64SubCommand::Encode(opts) => {
-                process_encode(&opts.input, opts.format)?;
-            }
-            Base64SubCommand::Decode(opts) => {
-                process_decode(&opts.input, opts.format)?;
-            }
-        },
-    }
-    Ok(())
+use axum::{Json, Router};
+use axum::routing::get;
+use serde::Serialize;
+
+#[derive(Serialize, Debug)]
+struct BlogPost{
+    id: i32,
+    title: String,
+    content: String,
+    tags: Vec<String>, // (标签数组，比如 ["Rust", "Web"])
+    is_published: bool,
+}
+async fn get_post() -> Json<BlogPost>{
+    Json(BlogPost{
+        id: 1,
+        title: "Rust 入门".to_string(),
+        content: "这是一个关于 Rust 的入门教程".to_string(),
+        tags: vec!["Rust".to_string(), "Web".to_string()],
+        is_published: true,
+    })
 }
