d0c0d88eeb44f44728b0a465bc48a85da1d3c2ba
汝吾此便安好 <sonder.qicat@outlook.com>
2026-04-12 00:55:49 +0800

    fate: Z S G C

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,9 +1,13 @@
-use axum::{extract::State, Json};
+use crate::error::AppError;
+use axum::{
+    Json,
+    extract::{Path, State},
+};
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
-use crate::error::AppError;
 
 #[derive(Serialize, Debug, sqlx::FromRow)]
+// 博客文章结构体
 pub struct BlogPost {
     pub id: i32,
     pub title: String,
@@ -11,16 +15,24 @@ pub struct BlogPost {
     pub tags: Vec<String>,
     pub is_published: bool,
 }
-
+// 更新博客文章请求体
+#[derive(Deserialize, Debug)]
+pub struct UpdatePostRequest {
+    pub title: String,
+    pub content: String,
+    pub tags: Vec<String>,
+}
+// 创建博客文章请求体
 #[derive(Deserialize, Debug)]
 pub struct CreatePostRequest {
     pub title: String,
     pub content: String,
     pub tags: Vec<String>,
 }
+// 创建博客文章处理函数
 pub async fn create_post(
-     State(pool): State<PgPool>,
-     Json(request): Json<CreatePostRequest>,
+    State(pool): State<PgPool>,
+    Json(request): Json<CreatePostRequest>,
 ) -> Result<Json<BlogPost>, AppError> {
     let post = sqlx::query_as!(
         BlogPost,
@@ -35,10 +47,45 @@ pub async fn create_post(
 
     Ok(Json(post))
 }
-
+// 获取博客文章处理函数
 pub async fn get_post(State(pool): State<PgPool>) -> Result<Json<Vec<BlogPost>>, AppError> {
     let posts = sqlx::query_as!(BlogPost, r#"select * from blog_posts"#,)
         .fetch_all(&pool)
         .await?;
     Ok(Json(posts))
+}
+// 更新博客文章处理函数
+// 注意参数里的 Path(id): Path<i32>，它会自动把网址里的 /api/post/5 里的 5 抠出来变成数字！
+pub async fn update_post(
+    State(pool): State<PgPool>,
+    Path(id): Path<i32>,
+    Json(request): Json<UpdatePostRequest>,
+) -> Result<Json<BlogPost>, AppError> {
+    let post = sqlx::query_as!(
+        BlogPost,
+        r#"
+update blog_posts set title = $1, content =$2, tags = $3 where id = $4 returning *
+"#,
+        request.title,
+        request.content,
+        &request.tags,
+        id
+    )
+    .fetch_one(&pool)
+    .await?;
+    Ok(Json(post))
+}
+// 删除博客文章处理函数
+pub async fn delete_post(
+    State(pool): State<PgPool>,
+    Path(id): Path<i32>,
+) -> Result<Json<serde_json::Value>, AppError> {
+    sqlx::query!(r#"delete from blog_posts where id = $1"#, id)
+        .bind(id)
+        .execute(&pool)
+        .await?;
+    // 删除成功后，没有具体的文章可以返回了，我们就手动拼一个友好的 JSON 返回去
+    Ok(Json::from(
+        serde_json::json!({"success": true, "message": format!("文章 {} 已经被彻底抹杀", id)}),
+    ))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 mod error;
-use axum::routing::{ get, post};
-use axum::{Router};
-use tower_http::cors::{Any, CorsLayer};
+use axum::routing::{get, post, put};
+use axum::Router;
+use rcli::handlers;
 use rcli::handlers::{create_post, get_post};
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
-async fn main() { // 🌟 去掉返回值
+async fn main() {
+    // 🌟 去掉返回值
     dotenvy::dotenv().ok();
 
     // 启动阶段的错误，直接原地爆炸并给出清晰的报错
-    let db_url = std::env::var("DATABASE_URL")
-        .expect("致命错误：找不到 DATABASE_URL 环境变量！");
+    let db_url = std::env::var("DATABASE_URL").expect("致命错误：找不到 DATABASE_URL 环境变量！");
 
     let pool = sqlx::postgres::PgPoolOptions::new()
         .connect(&db_url)
@@ -25,14 +26,18 @@ async fn main() { // 🌟 去掉返回值
     let app = Router::new()
         .route("/api/get_post/", get(get_post))
         .route("/api/create_post/", post(create_post))
+        .route(
+            "/api/update_post/{id}",
+            put(handlers::update_post).delete(handlers::delete_post),
+        )
         .with_state(pool)
         .layer(cors);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
-        .expect("致命错误：8080 端口被占用！");
+        .expect("致命错误：3000 端口被占用！");
 
-    println!("🚀 服务器已启动，请访问 http://localhost:8080");
+    println!("🚀 服务器已启动，请访问 http://localhost:3000");
 
     axum::serve(listener, app)
         .await
